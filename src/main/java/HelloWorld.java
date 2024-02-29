e59a7803a1ac51edd0ab9ca4ee2cc18792cddea8
Antonio Nogues <anoguesgomez@safareyes.es>
2024-02-29 16:15:36 +0100

    Cambios en HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,8 @@
+import java.time.LocalDateTime;
+
 public class HelloWorld {
     public static void main(String[] args) {
             System.out.println("Hola Mundo!");
+            System.out.println(LocalDateTime.now());
     }
 }
